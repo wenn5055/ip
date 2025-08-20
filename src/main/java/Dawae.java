0fa6d17e05ca0afcc577ae5af191bf44c6479062
wendy <wendytangxd@gmail.com>
2025-08-21 01:27:11 +0800

    Rename Duke.java to Dawae.java, Implement an initial skeletal version

---
+++ src/main/java/Dawae.java
@@ -0,0 +1,10 @@
+public class Dawae {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm DAWAE \uD83D\uDE06\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. \uD83D\uDC4B Hope to see you again soon! \uD83D\uDD25\n" +
+                "____________________________________________________________\n");
+    }
+}
