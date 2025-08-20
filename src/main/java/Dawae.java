c8397a637eeaed014405c04aea7e720feaf7d00f
wendy <wendytangxd@gmail.com>
2025-08-21 02:41:29 +0800

    Echo user input

--- src/main/java/Dawae.java
+++ src/main/java/Dawae.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
 public class Dawae {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
+        Scanner sc = new Scanner(System.in);
+        System.out.println(
+                "____________________________________________________________\n" +
                 " Hello! I'm DAWAE \uD83D\uDE06\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________\n");
+        String inputt = sc.nextLine();
+        System.out.println("____________________________________________________________\n");
+        while (!inputt.equals("bye")) {
+            System.out.println(inputt + "\n____________________________________________________________\n");
+            inputt = sc.nextLine();
+            System.out.println("____________________________________________________________\n");
+        }
+        System.out.println(
                 " Bye. \uD83D\uDC4B Hope to see you again soon! \uD83D\uDD25\n" +
                 "____________________________________________________________\n");
+        sc.close();
     }
 }
