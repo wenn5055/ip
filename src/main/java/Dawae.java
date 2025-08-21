6d0f9a1ec52470689736cc4b9a9b609de5834e0e
wendy <wendytangxd@gmail.com>
2025-08-21 10:09:58 +0800

    Clean up code
    Replaced horizontal lines with a final string representation for neatness.
    Replaced while loop with do while.

--- src/main/java/Dawae.java
+++ src/main/java/Dawae.java
@@ -5,31 +5,34 @@ public class Dawae {
         Scanner sc = new Scanner(System.in);
         String[] storage = new String[100];
         int i = 0;
+        final String line = "____________________________________________________________";
+        String inputt;
         
         System.out.println(
-                "____________________________________________________________\n" +
+                line + "\n" +
                 " Hello! I'm DAWAE \uD83D\uDE06\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n");
-        String inputt = sc.nextLine();
+                line);
         
-        System.out.println("____________________________________________________________");
-        while (!inputt.equals("bye")) {
-            if (!inputt.equals("list")) {
-                storage[i++] = inputt;
-                System.out.println("added: " + inputt);
-            } else {
-                for (int j = 1; j <= i; j++) {
-                    System.out.println(j + ". " + storage[j-1]);
-                }
-            }
-            System.out.println("____________________________________________________________");
+        do {
             inputt = sc.nextLine();
-            System.out.println("____________________________________________________________");
-        }
+            System.out.println(line);
+            if (!inputt.equals("bye")) {
+                if (inputt.equals("list")) {
+                    for (int j = 1; j <= i; j++) {
+                        System.out.println(j + ". " + storage[j - 1]);
+                    }
+                } else {
+                    storage[i++] = inputt;
+                    System.out.println("added: " + inputt);
+                }
+                System.out.println(line);
+            }
+        } while (!inputt.equals("bye"));
+        
         System.out.println(
-                " Bye. \uD83D\uDC4B Hope to see you again soon! \uD83D\uDD25\n" +
-                "____________________________________________________________");
+                "Bye. \uD83D\uDC4B Hope to see you again soon! \uD83D\uDD25\n" +
+                line);
         
         sc.close();
     }
